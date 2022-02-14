ad84a26741a0ae367fbf521790aee75cad0424d7
Kevin Erskine <kevinsam.erskine@gmail.com>
2022-02-14 15:34:48 -0500

    Updated code

--- Jeopardy.c
+++ Jeopardy.c
@@ -22,9 +22,102 @@
     //been answered correctly
     int counter = 0;
     
-void EndGame(){
+    //Stores the players names
+    char p1[10];
+    char p2[10];
+    char p3[10];
+    char p4[10];
     
-    printf("Player Scores: ");
+    //Keeps track of players scores
+    int Score1 = 0;
+    int Score2 = 0;
+    int Score3 = 0;
+    int Score4 = 0;
+    
+    //Keeps the order the players must follow
+    int playerOrder = 1;
+    
+
+void getPlayers(){
+    
+    //Gets each of the players names
+    printf("Enter name for player 1:  \n");
+    scanf("%s", p1);
+    
+    printf("Enter name for player 2:  \n");
+    scanf("%s", p2);
+    
+    printf("Enter name for player 3:  \n");
+    scanf("%s", p3);
+    
+    printf("Enter name for player 4:  \n");
+    scanf("%s", p4);
+    
+}
+
+int choosePlayer(){
+    
+    //play holds holds the value of which player's turn it is
+    int play = 0;
+    
+    //Player is used to recieve the player's name for validation
+    char Player[10];
+    
+    //Prints out which player's turn it is
+    printf("It is player ");
+    printf("%d", playerOrder);
+    printf(" turn!\n");
+    
+    //Recieves players name for validation
+    printf("Enter player's name for validation: \n");
+    scanf("%s", Player);
+    printf("\n");
+    
+    //Checks if correct player is playing and sets play to appropriate player
+    if((strcmp(Player, p1) == 0) && playerOrder == 1){
+        play = 1;
+        printf("Player 1 may continue! \n");
+    } else if ((strcmp(Player, p2) == 0) && playerOrder == 2){
+        play = 2;
+        printf("Player 2 may continue! \n");
+    } else if ((strcmp(Player, p3) == 0) && playerOrder == 3){
+        play = 3;
+        printf("Player 3 may continue! \n");
+    } else if ((strcmp(Player, p4) == 0) && playerOrder == 4){
+        play = 4;
+        printf("Player 4 may continue! \n");
+    } else{
+        printf("Not a valid player name!\n");
+        choosePlayer();
+    }
+    
+    //Returns which players turn it is
+    return play;
+}
+
+void printScores(){
+    
+    //Prints out each players score
+    printf("%s",p1);
+    printf(" score is: ");
+    printf("%d",Score1);
+    printf("\n\n");
+    
+    printf("%s",p2);
+    printf(" score is: ");
+    printf("%d",Score2);
+    printf("\n\n");
+    
+    printf("%s",p3);
+    printf(" score is: ");
+    printf("%d",Score3);
+    printf("\n\n");
+    
+    printf("%s",p4);
+    printf(" score is: ");
+    printf("%d",Score4);
+    printf("\n\n");
+    
 }
     
 void CreateBoard(){
@@ -33,8 +126,13 @@ void CreateBoard(){
     if (counter == 9){
         
         printf("All questions have been answered!\n");
-        EndGame();
+        printScores();
         return 0;
+    }
+    
+    //Resets turn back to player 1 after player 4's turn is over
+    if (playerOrder > 4){
+        playerOrder = 1;
     }
     
     //Print catagories
@@ -57,9 +155,13 @@ void CreateBoard(){
         printf("\n");
     }
     
-    //Value from Compare function is sent to Ques function
+    //Gets the values from both functions 
+    int val2 = choosePlayer();
+    
     int val = Compare();
-    Ques(val);
+    
+    //Value from Compare function and choosePlayer function is sent to Ques function
+    Ques(val, val2);
     
 }
 
@@ -123,7 +225,7 @@ int Compare(){
     
 }
 
-void Ques(int x){
+void Ques(int x, int y){
     
     //ans holds the ans from the user
     char ans[10];
@@ -153,12 +255,26 @@ void Ques(int x){
             m1 = 0;
             x1 = 1;
             counter++;
+            playerOrder++;
+            
+            //Updates score of appropriate player
+            if (y == 1){
+                Score1 += 100;
+            }else if(y == 2){
+                Score2 += 100;
+            }else if(y == 3){
+                Score3 += 100;
+            } else {
+                Score4 += 100;
+            }
+            
             CreateBoard();
             
         } else {
             
             //Wrong answer
             printf("Wrong\n");
+            playerOrder++;
             CreateBoard();
             
         }
@@ -182,11 +298,24 @@ void Ques(int x){
             x2 = 1;
             m2 = 0;
             counter++;
+            playerOrder++;
+            
+            if (y == 1){
+                Score1 += 200;
+            }else if(y == 2){
+                Score2 += 200;
+            }else if(y == 3){
+                Score3 += 200;
+            } else {
+                Score4 += 200;
+            }
+            
             CreateBoard();
             
         } else {
             
             printf("Wrong\n");
+            playerOrder++;
             CreateBoard();
             
         }
@@ -209,11 +338,24 @@ void Ques(int x){
             x3 = 1;
             m3 = 0;
             counter++;
+            playerOrder++;
+            
+            if (y == 1){
+                Score1 += 300;
+            }else if(y == 2){
+                Score2 += 300;
+            }else if(y == 3){
+                Score3 += 300;
+            } else {
+                Score4 += 300;
+            }
+            
             CreateBoard();
             
         } else {
             
             printf("Wrong\n");
+            playerOrder++;
             CreateBoard();
             
         }
@@ -239,11 +381,24 @@ void Ques(int x){
             x4 = 1;
             s1 = 0;
             counter++;
+            playerOrder++;
+            
+            if (y == 1){
+                Score1 += 100;
+            }else if(y == 2){
+                Score2 += 100;
+            }else if(y == 3){
+                Score3 += 100;
+            } else {
+                Score4 += 100;
+            }
+            
             CreateBoard();
         
         } else {
             
             printf("Wrong\n");
+            playerOrder++;
             CreateBoard();
             
         }
@@ -269,11 +424,24 @@ void Ques(int x){
             x5 = 1;
             s2 = 0;
             counter++;
+            playerOrder++;
+            
+            if (y == 1){
+                Score1 += 200;
+            }else if(y == 2){
+                Score2 += 200;
+            }else if(y == 3){
+                Score3 += 200;
+            } else {
+                Score4 += 200;
+            }
+            
             CreateBoard();
             
         } else {
             
             printf("Wrong\n");
+            playerOrder++;
             CreateBoard();
             
         }
@@ -297,11 +465,24 @@ void Ques(int x){
             x6 = 1;
             s3 = 0;
             counter++;
+            playerOrder++;
+            
+            if (y == 1){
+                Score1 += 300;
+            }else if(y == 2){
+                Score2 += 300;
+            }else if(y == 3){
+                Score3 += 300;
+            } else {
+                Score4 += 300;
+            }
+            
             CreateBoard();
         
         } else {
             
             printf("Wrong\n");
+            playerOrder++;
             CreateBoard();
             
         }
@@ -327,11 +508,24 @@ void Ques(int x){
             x7 = 1;
             g1 = 0;
             counter++;
+            playerOrder++;
+            
+            if (y == 1){
+                Score1 += 100;
+            }else if(y == 2){
+                Score2 += 100;
+            }else if(y == 3){
+                Score3 += 100;
+            } else {
+                Score4 += 100;
+            }
+            
             CreateBoard();
              
         } else {
             
             printf("Wrong\n");
+            playerOrder++;
             CreateBoard();
             
         }
@@ -356,11 +550,24 @@ void Ques(int x){
             x8 = 1;
             g2 = 0;
             counter++;
+            playerOrder++;
+            
+            if (y == 1){
+                Score1 += 200;
+            }else if(y == 2){
+                Score2 += 200;
+            }else if(y == 3){
+                Score3 += 200;
+            } else {
+                Score4 += 200;
+            }
+            
             CreateBoard();
               
         } else {
             
             printf("Wrong\n");
+            playerOrder++;
             CreateBoard();
             
         }
@@ -384,24 +591,42 @@ void Ques(int x){
             x9 = 1;
             g3 = 0;
             counter++;
+            playerOrder++;
+            
+            if (y == 1){
+                Score1 += 300;
+            }else if(y == 2){
+                Score2 += 300;
+            }else if(y == 3){
+                Score3 += 300;
+            } else {
+                Score4 += 300;
+            }
+            
             CreateBoard();
              
         } else {
             
             printf("Wrong\n");
+            playerOrder++;
             CreateBoard();
             
         }
     }
     
-    
-    
 }
+
+
 
 int main(){
     
+    //Prints welcome message
+    printf("Welcome to Jeopardy!\n\n");
+    printf("Order of players will go in numerical order\n\n\n");
+    
+    //Gets the players to start the game
+    getPlayers();
     //Create the board for the game Board
     CreateBoard();
 }    
-    
-    
+  
